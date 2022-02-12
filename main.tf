7413ca0927baada62e46e268ecae22eaffdfecba
Allan Taylor <allan@startmail.com>
2022-02-12 12:08:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,4 +15,18 @@ module "network-hub" {
     default_subnet_cidr           = var.default_subnet_cidr
     firewall_subnet_cidr          = var.firewall_subnet_cidr
     gateway_subnet_cidr           = var.gateway_subnet_cidr
+    vgw_active_active_mode        = var.vgw_active_active_mode
+    ptp_vpn_enable_bgp            = var.ptp_vpn_enable_bgp
+    ptp_vpn_sku                   = var.ptp_vpn_sku
+    ptp_vpn_psk                   = var.ptp_vpn_psk
+    ptp_vpn_remote_endpoint_name  = var.ptp_vpn_remote_endpoint_name
+    ptp_vpn_remote_endpoint_fqdn  = var.ptp_vpn_remote_endpoint_fqdn
+    ptp_vpn_remote_network        = var.ptp_vpn_remote_network
+    ptp_vpn_ike_encryption        = var.ptp_vpn_ike_encryption
+    ptp_vpn_ike_integrity         = var.ptp_vpn_ike_integrity
+    ptp_vpn_dh_group              = var.ptp_vpn_dh_group
+    ptp_vpn_ipsec_encryption      = var.ptp_vpn_ipsec_encryption
+    ptp_vpn_ipsec_integrity       = var.ptp_vpn_ipsec_integrity
+    ptp_vpn_pfs_group             = var.ptp_vpn_pfs_group
+    ptp_vpn_sa_lifetime           = var.ptp_vpn_sa_lifetime
 }
