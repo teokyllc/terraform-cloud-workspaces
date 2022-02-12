0c9263968e05a45683c4cc5bc214f07ddfa62f53
Allan Taylor <allan@startmail.com>
2022-02-12 12:01:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 module "network-hub" {
     source                        = "app.terraform.io/ANET/network-hub/azure"
     version                       = "1.0.1"
-    environment_tag               = "MGMT"
-    region                        = "EastUS"
-    hub_rg_name                   = "Hub-RG"
-    hub_vnet_name                 = "Hub-vNet"
-    hub_route_table_name          = "Hub-Route-Table"
-    default_subnet_name           = "default"
-    enable_p2p_vpn                = false
-    enable_subnet_nsg             = false
-    enable_subnet_firewall        = false
-    disable_bgp_route_propagation = false
-    vnet_cidr                     = "10.0.0.0/16"
-    default_subnet_cidr           = "10.0.0.0/24"
-    firewall_subnet_cidr          = "10.0.254.0/24"
-    gateway_subnet_cidr           = "10.0.255.0/24"
+    environment_tag               = var.environment_tag
+    region                        = var.region
+    hub_rg_name                   = var.hub_rg_name
+    hub_vnet_name                 = var.hub_vnet_name
+    hub_route_table_name          = var.hub_route_table_name
+    default_subnet_name           = var.default_subnet_name
+    enable_p2p_vpn                = var.enable_p2p_vpn
+    enable_subnet_nsg             = var.enable_subnet_nsg
+    enable_subnet_firewall        = var.enable_subnet_firewall
+    disable_bgp_route_propagation = var.disable_bgp_route_propagation
+    vnet_cidr                     = var.vnet_cidr
+    default_subnet_cidr           = var.default_subnet_cidr
+    firewall_subnet_cidr          = var.firewall_subnet_cidr
+    gateway_subnet_cidr           = var.gateway_subnet_cidr
 }
