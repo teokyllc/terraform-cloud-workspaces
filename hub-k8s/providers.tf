057bdc0837873533b2eff1ce4785442f4037fd4a
Allan Taylor <allan@startmail.com>
2022-03-16 09:54:24 -0400

    wip

--- hub-k8s/providers.tf
+++ hub-k8s/providers.tf
@@ -1,19 +1,19 @@
 provider "kubernetes" {
-  host                   = data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.host
-  username               = data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.username
-  password               = data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.password
-  client_certificate     = base64decode(data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.client_certificate)
-  client_key             = base64decode(data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.cluster_ca_certificate)
+  host                   = data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.host
+  username               = data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.username
+  password               = data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.password
+  client_certificate     = base64decode(data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.client_certificate)
+  client_key             = base64decode(data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.host
-    username               = data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.username
-    password               = data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.password
-    client_certificate     = base64decode(data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.client_certificate)
-    client_key             = base64decode(data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.hub_infra.aks.outputs.aks_kube_config.0.cluster_ca_certificate)
+    host                   = data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.host
+    username               = data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.username
+    password               = data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.password
+    client_certificate     = base64decode(data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.client_certificate)
+    client_key             = base64decode(data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.hub_infra.outputs.aks_kube_config.0.cluster_ca_certificate)
   }
 }
