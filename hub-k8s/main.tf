9d718255fa6b76e8b87dc0738b3b702c0caf572d
Allan Taylor <allan@startmail.com>
2022-03-19 08:00:35 -0400

    wip

--- hub-k8s/main.tf
+++ hub-k8s/main.tf
@@ -1,6 +1,6 @@
 module "cert-manager" {
   source                 = "app.terraform.io/ANET/cert-manager/kubernetes"
-  version                = "1.0.3"
+  version                = "1.0.4"
   cert_manager_namespace = "cert-manager"
   values_filename        = "cert-manager-values.yaml"
   enable_vault_issuer    = false
