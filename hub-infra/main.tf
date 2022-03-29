5fd418a861059722b25b89de76aea37fb711739c
Allan Taylor <allan@startmail.com>
2022-03-29 06:08:25 -0400

    wip

--- hub-infra/main.tf
+++ hub-infra/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "aks_rg" {
 
 module "network_hub" {
     source                        = "app.terraform.io/ANET/network-hub/azure"
-    version                       = "1.0.4"
+    version                       = "1.0.5"
     environment_tag               = local.environment_tag
     region                        = local.region
     hub_rg_name                   = azurerm_resource_group.network_rg.name
