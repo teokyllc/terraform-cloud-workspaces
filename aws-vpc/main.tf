7016c2950b74660c0e7bf23f9abab3067d118fab
Allan Taylor <allan@startmail.com>
2022-06-09 08:58:01 -0400

    wip

---
+++ aws-vpc/main.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+  source                 = "app.terraform.io/ANET/vpc/aws"
+  version                = "1.0.0"
+  vpc_name               = "KentuckyMyco"
+  cidr_block             = "10.0.0.0/16"
+  instance_tenancy       = "default"
+  enable_public_subnets  = true
+  enable_private_subnets = false
+  enable_dns_support     = true
+  enable_dns_hostnames   = true
+}
+
